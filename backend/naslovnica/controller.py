caa946065202063a16950c59e16a12ccdb66b5d5
ToninoK <tonino.kastelan@skole.hr>
2018-07-27 19:43:20 +0200

    Added data check for adding post

--- backend/naslovnica/controller.py
+++ backend/naslovnica/controller.py
@@ -1,4 +1,5 @@
 from flask import jsonify
+from datetime import datetime
 
 
 class DataHandler:
@@ -11,19 +12,92 @@ class DataHandler:
         )
         return json_data
 
-    def post_background_photo(self, photoURL):
-        data = self.db.find_one()
-        current = data
-        data['slika']['url'] = photoURL
-        self.db.replace_one(current, data)
+    def check_value(self, key, data):
+        options = {
+            "slika": self.change_back_photo,
+            "novost": self.add_new_post,
+            "uspjeh": self.add_new_achievement,
+            "faks": self.add_new_college,
+            "predmet": self.add_new_subject,
+            "konakt": self.add_new_contact,
+            "link": self.add_new_link
+        }
+        case = options.get(key, None)
+        if case is not None:
+            return case(data)
         return None
 
-    def post_new_post(self, post_data):
+    def change_back_photo(self, img_data):
         data = self.db.find_one()
         current = data
-        check = ["title", "body", "date", "publisher", "img"]
-        if len(check) == len([keyA for keyA, keyB in zip(check, post_data.keys()) if keyA == keyB]):
-            data['novosti'].append(post_data)
+        if type(img_data["url"]) == str:
+            data['slika']['url'] = img_data
             self.db.replace_one(current, data)
+        return None
+
+    def add_new_post(self, post_data):
+        data = self.db.find_one()
+        current = data
+        check = {
+            "title": "string",
+            "body": "body",
+            "date": datetime.now(),
+            "publisher": "publisher",
+            "img": "img"
+        }
+
+        try:
+            date = parse_date(post_data["date"])
+            post_data["date"] = date
+
+        except ValueError:
+            return None
+
+        try:
+            valid = True
+            for key in post_data:
+                if type(check[key]) != type(post_data[key]):
+                    valid = False
+            if valid:
+                data['novosti'].append(post_data)
+                self.db.replace_one(current, data)
+
+        except KeyError:
+            return None
 
         return None
+
+    def add_new_achievement(self, data):
+        pass
+
+    def add_new_college(self, data):
+        pass
+
+    def add_new_subject(self, data):
+        pass
+
+    def add_new_contact(self, data):
+        pass
+
+    def add_new_link(self, data):
+        pass
+
+
+def parse_date(text_date):
+
+    dict_month = {
+        "Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5,
+        "Jun": 6, "Jul": 7, "Aug": 8, "Sept": 9, "Oct": 10,
+        "Nov": 11, "Dec": 12
+    }
+
+    lst_date = text_date.split()
+    lst_time = lst_date[4].split(':')
+
+    yyyy = int(lst_date[3])
+    mm = int(dict_month[lst_date[1]])
+    dd = int(lst_date[2])
+    h = int(lst_time[0])
+    _min = int(lst_time[1])
+
+    return datetime(yyyy, mm, dd, h, _min)
