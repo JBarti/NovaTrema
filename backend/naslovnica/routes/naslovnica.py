fa13c713310d7d471cb8c607b2aea7a121b8c7f5
ToninoK <tonino.kastelan@skole.hr>
2018-09-18 11:23:01 +0200

    Added docs for the homepage

--- backend/naslovnica/routes/naslovnica.py
+++ backend/naslovnica/routes/naslovnica.py
@@ -32,32 +32,65 @@ def naslovnica():
     return jsonify(data)
 
 
-@naslovnica_bp.route('/<value>', methods=['POST', 'PUT', 'DELETE'])
-def elementi(value):
-    """Route whose return value varies depending on value and method,
-    it also makes a corresponding function call
+@naslovnica_bp.route('/<element>', methods=['POST'])
+def post_elements(element):
+    """Route used for posting data.
+    Data which is received on this route is a dictionary containing
+    the data concerning the element which is being posted.
 
     Arguments:
-        value {str} -- url suffix which defines the route and function call
+        element {str} -- text value which defines which element will be posted
 
     Returns:
-        <class flask.Response> -- jsonified data
+        <class flask.Response> or html status code -- jsonified data which has been received in the first place.
+        The data is returned in case of no errors, and the html status code in case of an error
     """
 
     data = request.get_json()
-    if request.method == 'POST':
-        data_handler = PostHandler(MONGO.db)
-        func = data_handler.call_function(value, data)
-        if func is not None:
-            return jsonify_objectId(func)
-    if request.method == 'PUT':
-        data_handler = PutHandler(MONGO.db)
-        func = data_handler.call_function(value, data)
-        if func is not None:
-            return jsonify_objectId(func)
-    if request.method == 'DELETE':
-        data_handler = DeleteHandler(MONGO.db)
-        func = data_handler.call_function(value, data)
-        if func is not None:
-            return jsonify_objectId(func)
+    data_handler = PostHandler(MONGO.db)
+    dict_data = data_handler.call_function(element, data)
+    if isinstance(dict_data, dict):
+        return jsonify_objectId(dict_data)
+    return abort(400, "This is a non existant route")
+
+
+@naslovnica_bp.route('/<element>', methods=['PUT'])
+def put_elements(element):
+    """Route used for updating data.
+    Data which is received on this route is a dictionary containing
+    the data concerning the element which is being updated.
+
+    Arguments:
+        element {str} -- text value which defines which element will be updated
+
+    Returns:
+        <class flask.Response> or html status code -- jsonified data which has been received in the first place.
+        The data is returned in case of no errors, and the html status code in case of an error
+    """
+    data = request.get_json()
+    data_handler = PutHandler(MONGO.db)
+    dict_data = data_handler.call_function(element, data)
+    if isinstance(dict_data, dict):
+        return jsonify_objectId(dict_data)
+    return abort(400, "This is a non existant route")
+
+
+@naslovnica_bp.route('/<element>', methods=['DELETE'])
+def delete_elements(element):
+    """Route used for deleteing data.
+    Data which is received on this route is a dictionary containing
+    the data concerning the element which is being deleted
+
+    Arguments:
+        element {str} -- text value which defines which element will be deleted.
+
+    Returns:
+        <class flask.Response> or html status code -- jsonified data which has been received in the first place.
+        The data is returned in case of no errors, and the html status code in case of an error
+    """
+    data = request.get_json()
+    data_handler = DeleteHandler(MONGO.db)
+    dict_data = data_handler.call_function(element, data)
+    if isinstance(dict_data, dict):
+        return jsonify_objectId(dict_data)
     return abort(400, "This is a non existant route")
