5d5fb7091aff4d49329d3b2738886db0cff27f79
MatejKozica <matej.split@gmail.com>
2018-10-08 16:00:50 +0200

    Added Novosti route and page

---
+++ frontend/src/pages/novosti/Novosti.jsx
@@ -0,0 +1,10 @@
+import React, { component } from React
+
+class Novosti extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+export default Novosti
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,11 +3,13 @@ import { BrowserRouter } from 'react-router-dom'
 import { Route, Switch } from 'react-router'
 import App from './App.jsx'
 import Naslovnica from './pages/naslovnica/Naslovnica.jsx'
+import Novosti from './pages/novosti/Novosti.jsx'
 
 export default (
     <BrowserRouter>
         <Switch>
             <Route exact path='/naslovnica' component={Naslovnica} />
+            <Route exact path='/novosti' component={Novosti} />
             <Route exact path='/test' component={App} />
         </Switch>
     </BrowserRouter>
